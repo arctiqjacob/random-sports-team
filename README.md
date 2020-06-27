746f7e6438a985ef64ccb9971cc533b1a0f33647
jacob <jacob.mammoliti@arctiq.ca>
2020-06-26 23:08:44 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Random Sports Team
+Demo Python application showing random NHL Sports teams.
+
+## How to Build
+```shell
+$ docker build --tag random-sports-team .
+
+$ docker tag random-sports-team jacobmammoliti/random-sports-team
+
+$ docker push jacobmammoliti/random-sports-team
+```
+
+## Deploy into Kubernetes
+```shell
+$ kubectl create deployment random-sports-team --image=jacobmammoliti/random-sports-team
+
+$ kubectl expose deployment random-sports-team --port 8080
+
+$ kubectl port-forward svc/random-sports-team 8080:
+```
---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask, render_template
+from random import randrange
+import requests
+import json
+
+app = Flask(__name__)
+
+NHLAPI = "https://statsapi.web.nhl.com/api/v1/teams/"
+
+@app.route('/')
+def index():
+    response = requests.get(NHLAPI + str(randrange(1,30))).json()
+    team_name = response['teams'][0]['name'].replace(" ", "_").lower()
+
+    return render_template('index.html', response=response, team_name=team_name)
+
+@app.route('/health')
+def healthz():
+    return json.loads('{ "status":"OK" }')
+
+if __name__ == "__main__":
+    app.run(debug=os.environ.get('DEBUG', False),
+            host=os.environ.get('HOST', '0.0.0.0'),
+            port=os.environ.get('PORT', 8080))
